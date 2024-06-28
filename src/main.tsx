a283db4c23a4f0b1108865dfa45b159e9bb13794
Germain <germain.gg90@gmail.com>
2024-06-27 21:49:51 -0400

    add tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router/router.tsx';
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router/router.tsx";
 
-import './index.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <main className="dark text-foreground bg-background">
-        <RouterProvider router={ router } />
-      </main>
-    </NextUIProvider>
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <RouterProvider router={router} />
+        </main>
+      </NextUIProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
