52e5704b072d26d1d42fba0a8c126f8969ef8efc
Germain <germain.gg90@gmail.com>
2024-06-27 22:22:16 -0400

    axios + fetching

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,17 @@ import { NextUIProvider } from "@nextui-org/react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.tsx";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
-
-const queryClient = new QueryClient();
+import { TanStackProvider } from "./plugins/TanStackProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <TanStackProvider>
       <NextUIProvider>
         <main className="dark text-foreground bg-background">
           <RouterProvider router={router} />
         </main>
       </NextUIProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </TanStackProvider>
   </React.StrictMode>
 );
--- src/products/index.ts
+++ src/products/index.ts
@@ -1,14 +1,12 @@
+export { ProductCard } from "./components/ProductCard";
+export { ProductList } from "./components/ProductList";
 
+export { StoreLayout } from "./layout/StoreLayout";
 
+export { CompleteListPage } from "./pages/CompleteListPage";
+export { MensPage } from "./pages/MensPage";
+export { NewProduct } from "./pages/NewProduct";
+export { WomensPage } from "./pages/WomensPage";
 
-export { ProductCard } from './components/ProductCard';
-export { ProductList } from './components/ProductList';
-
-
-export { StoreLayout } from './layout/StoreLayout';
-
-
-export { CompleteListPage } from './pages/CompleteListPage';
-export { MensPage } from './pages/MensPage';
-export { NewProduct } from './pages/NewProduct';
-export { WomensPage } from './pages/WomensPage';
+export { productsApi } from "./api/productsApi";
+export type { Product } from "./interfaces/products";
